9531c6e81b07b48f8ed22e314085aba705dad20d
dsat-2005 <medo01020709093@gmail.com>
2025-10-11 17:48:06 +0300

    Updated DashboardLayout.tsx

--- components/DashboardLayout.tsx
+++ components/DashboardLayout.tsx
@@ -34,8 +34,8 @@ export function DashboardLayout({ children }: DashboardLayoutProps) {
                   <Image
                     src="/logo.png"
                     alt="Logo"
-                    width={64}
-                    height={64}
+                    width={100}
+                    height={100}
                     className="object-contain"
                   />
                 </div>
